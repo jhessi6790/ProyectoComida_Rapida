31d1339679612e9400f360352053120cb9f15ece
Jhessi <jhessicavalle@gmail.com>
2020-10-22 01:28:05 -0400

    validaciones

--- database/collection/menu.js
+++ database/collection/menu.js
@@ -2,9 +2,18 @@ const mongoose = require("../connect");
 var mon = require('mongoose');
 var Schema = mon.Schema;
 var menusSchema = new Schema({
-  name : String,
-  precio : Number,
-  description : String,
+  name : {
+    type: String,
+    required: [true, 'debe poner un nombre']
+  },
+  precio : {
+    type: Number,
+    required: [true, 'debe poner el precio']
+  },
+  description : {
+    type: String,
+    required: [true, 'debe poner la descripcion']
+  },
   registerdate : Date,
   picture: String,
   restaurant:{
